3ed99b00e06ba748ef18bfcab51ab7850e6dc26e
harsargsyan <harutyunsargsyan@outlook.com>
2023-12-03 23:13:32 +0400

    add new MyArray class

---
+++ Array/MyArray.cs
@@ -0,0 +1,37 @@
+namespace Array
+{
+    class MyArray<T>
+    {
+        private T[] array;
+
+        public MyArray(int size)
+        {
+            array = new T[size];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+    }
+}
